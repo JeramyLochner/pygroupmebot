a438f735999c42a6827a1ab0871a9bb72127d0ff
Jeramy Lochner <jerloch@gmail.com>
2016-10-03 16:16:34 -0700

    More precise command parsing

--- bot.py
+++ bot.py
@@ -20,17 +20,17 @@ botuserid = config['BOT']['user_id']
 
 def runcommand(message):
 	command = message.strip()
-	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+	process = subprocess.Popen(command.split()[1:], stdout=subprocess.PIPE)
 	output, error = process.communicate()
-	return output
+	return str(output)
 
 message_replies = {'hello bot': ['Hi!', 'Hello!', 'Waddup'], '/test': 'test', '/command': runcommand}
 
 @app.route('/', method='POST')
 def listener():
 	global botid, groupid, botuserid
-
 	print(botid, " ", groupid, " ", botuserid)
+
 	group, message = get_last_message(str(groupid))
 	bot = get_bot(str(botid))
 	print(message.user_id, "-", message)
@@ -42,6 +42,13 @@ def listener():
 				bot.post(random.choice(message_replies[message.text]))
 			else:
 				bot.post(message_replies[message.text])
+		else:
+			command = message.text.split(' ')[0]
+			print(command)
+			if command in message_replies:
+				if callable(message_replies[command]):
+					bot.post(message_replies[command](message.text))
+
 	return message
 
 def get_last_message(groupid):
